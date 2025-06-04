bc93148b56c35dfef3da983fd305a950ced1cbb2
Leon Daniel Braungardt <leon_daniel.braungardt@smail.th-koeln.de>
2025-06-04 16:55:06 +0200

    adjust setup py to new module directory name (src)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Real-time ADC reader for xWRL6432 mmWave radar via DCA1000",
     author="Leon Braungardt",
-    packages=find_packages(exclude=["examples", "radar_config", "images"]),
+    packages=find_packages(where='src', exclude=["examples", "radar_config", "images"]),
     install_requires=[
         "numpy",
         "pyserial",
